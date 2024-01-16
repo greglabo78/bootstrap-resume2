242f31af08c56779628452957d61f28473c1ba81
greglabo78 <>
2024-01-16 04:45:15 +0000

    Fix bugs for the gh-user-data & gh-repo-data elements

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -6,34 +6,39 @@ function userInformationHTML(user) {
             </span>
         </h2>
         <div class="gh-content">
-        <div class="gh-Avatar">
-            <a href="${user.html_url} target="_blank>
-                <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
-            </a>
-        </div>
-            <P>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos} </P>
+            <div class="gh-avatar">
+                <a href="${user.html_url}" target="_blank">
+                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
+                </a>
+            </div>
+            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
         </div>`;
 }
 
-function repoInformation(repos) {
+function repoInformationHTML(repos) {
     if (repos.length == 0) {
         return `<div class="clearfix repo-list">No repos!</div>`;
     }
 
-    var listItemsHTML = repos.map(function(repo) {
+    var listItemsHTML = repos.map(function (repo) {
         return `<li>
-                    <a href="${repo.html_url}" target="_blank>${repo.name}</a>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
                 </li>`;
     });
 
     return `<div class="clearfix repo-list">
-                <p><strong>Repo List:</strong></p>
-                <ul> ${listItemsHTML.join("\n")}</ul>
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}
+                </ul>
             </div>`;
 }
 
-
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
 
     var username = $("#gh-username").val();
     if (!username) {
@@ -43,24 +48,29 @@ function fetchGitHubInformation(event) {
 
     $("#gh-user-data").html(
         `<div id="loader">
-        <img src="assets/css/loader.gif" alt="loading...." />
+            <img src="assets/css/loader.gif" alt="loading..." />
         </div>`);
 
-        $.when(
-            $.getJSON(`https://api.github.com/users/${username}`),
-            $.getJSON(`https://api.github.com/users/${username}/repos`)
-        ).then(
-            function(firstResponse, secondResponse) {
-                var userData = firstResponse[0];
-                var repoData = secondResponse[0];
-                $("#gh-user-data").html(userInformationHTML(userData));
-                $("#gh-repo-data").html(userInformationHTML(repoData));
-            }, function(errorResponse){
-                if (errorResponse.status === 404) {
-                    $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
-                } else {
-                    console.log(errorResponse);
-                    $("#gh-user-data").html(`<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
-                }
-            });
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
+    ).then(
+        function (firstResponse, secondResponse) {
+            var userData = firstResponse[0];
+            var repoData = secondResponse[0];
+            $("#gh-user-data").html(userInformationHTML(userData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
+        },
+        function (errorResponse) {
+            if (errorResponse.status === 404) {
+                $("#gh-user-data").html(
+                    `<h2>No info found for user ${username}</h2>`);
+            } else {
+                console.log(errorResponse);
+                $("#gh-user-data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+            }
+        });
 }
+
+$(document).ready(fetchGitHubInformation);
